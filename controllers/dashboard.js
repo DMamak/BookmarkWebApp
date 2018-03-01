55d5b53e898a84c3c035495eb88be198264e9622
Glitch (woozy-triangle) <none>
2018-03-01 14:35:35 +0000

    🐂🚒 Checkpoint views/partials/menu.hbs:653018/29 views/about.hbs:653018/30 views/start.hbs:653018/157 controllers/about.js:653018/33 controllers/start.js:653018/34 controllers/dashboard.js:653018/41 views/partials/mainpanel.hbs:653018/26 views/partials/bookmarkslist.hbs:653018/114

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -6,7 +6,7 @@ const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
-      title: 'Template 1 Dashboard',
+      title: 'Bookmarks Dashboard',
     };
     response.render('dashboard', viewData);
   },
