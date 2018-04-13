9142c3a44f559508632e0c11e13d2b5db530c613
Glitch (assigment1-dm) <none>
2018-04-13 09:26:58 +0000

    Added function to leave comments in about us

--- controllers/about.js
+++ controllers/about.js
@@ -1,6 +1,7 @@
 'use strict';
 const accounts = require('./accounts.js');
 const logger = require('../utils/logger');
+const messageStore = require('../models/messageStore.js');
 
 const about = {
   index(request, response) {
@@ -10,11 +11,22 @@ const about = {
     const viewData = {
       title: 'About Bookmarks App',
       fullname: loggedInUser.firstName+' '+loggedInUser.lastName,
+      messages:messageStore.getAllMessages(),
     };
     response.render('about', viewData);
     }
     else response.redirect('/');
   },
+  
+  addMessage(request,response){
+   const message = request.body.message;
+    const newMessage = {
+      messagetext:message,
+    };
+    messageStore.addMessage(newMessage);
+    response.redirect('/about');
+  },
+  
 };
 
 module.exports = about;
